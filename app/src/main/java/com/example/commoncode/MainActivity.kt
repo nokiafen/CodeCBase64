ebbb641bcfa7513c494ba86d261c632cfb238746
nokiafen <1093815412@qq.com>
2023-04-10 15:02:56 +0800

    gradle-versin changed

--- app/src/main/java/com/example/commoncode/MainActivity.kt
+++ app/src/main/java/com/example/commoncode/MainActivity.kt
@@ -1,40 +1,20 @@
 package com.example.commoncode
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+
 import android.view.Menu
 import android.view.MenuItem
-import com.example.commoncode.databinding.ActivityMainBinding
 import org.apaches.commons.codec.binary.Base64
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_main)
         val base64= Base64();
-        val bytes =base64.decode("")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -51,11 +31,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
